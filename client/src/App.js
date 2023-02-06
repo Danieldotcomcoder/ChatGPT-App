71c9cd126c13ea2beab86ce3800619d74a6cd98f
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-02-06 19:12:12 +0200

    readd all files

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import './normal.css'
+function App() {
+  return (
+    <div className="App">
+      <aside className="sidemenu">
+        <h1>Aside</h1>
+      </aside>
+
+
+
+      <section className="chatbox">
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
