e2c1fd3780c7a00bb135ec72331e9e0b6e5d04c0
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-02-07 09:54:56 +0200

    Add Text Input area and style it

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,24 @@ function App() {
   return (
     <div className="App">
       <aside className="sidemenu">
-        <h1>Aside</h1>
-      </aside>
+         <div className="side-menu-button">
+            <span>+</span>
+             New Chat
+         </div>
+          </aside>
 
 
 
       <section className="chatbox">
+        <div className="chat-input-holder">
+          <textarea 
+          rows="1"
+          className="chat-input-textarea"
+          >
 
+          </textarea>
+
+        </div>
       </section>
     </div>
   );
