b21950606082c9f38d13c70966559801bbf61d0d
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-02-07 12:50:07 +0200

    create express app for get and post

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Configuration, OpenAIApi } = require('openai');
+const express = require('express')
 
 const configuration = new Configuration({
   organization: 'org-ialr7176xUE4myKTzcivbiBn',
@@ -6,15 +7,25 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-async function callApi() {
-  const response = await openai.createCompletion({
-    model: 'text-davinci-003',
-    prompt: 'Say this is a test',
-    max_tokens: 7,
-    temperature: 0,
-  });
 
-  console.log(response.data.choices[0].text);
-}
-callApi()
+const app = express()
+const port = 3000
 
+app.get('/', async(req,res) => {
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: 'Say this is a test',
+        max_tokens: 7,
+        temperature: 0,
+      });
+    
+      console.log(response.data.choices[0].text);
+
+      res.json({
+        data: response.data
+      })
+})
+
+app.listen(port, () => {
+    console.log('Example app listening at http://localhost:${port}')
+})
