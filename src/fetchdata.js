86c3a5b50b968a493c32e1b1f7cebb77eddea7d7
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-02-10 12:07:37 +0200

    add env var

--- src/fetchdata.js
+++ src/fetchdata.js
@@ -1,6 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 var ApiKey = process.env.API_KEY;
+console.log(ApiKey)
 const configuration = new Configuration({
     organization: 'org-ialr7176xUE4myKTzcivbiBn',
     apiKey: ApiKey,
