b4cb871ab439421c43c50020d9baeb67db2062ed
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2023-03-15 14:12:10 +0200

    Implemented chatting with chat gpt3.5

--- src/fetchdata.js
+++ src/fetchdata.js
@@ -1,27 +1,39 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 import { ApiKey } from './api';
 
 const configuration = new Configuration({
-    organization: 'org-ialr7176xUE4myKTzcivbiBn',
-    apiKey: ApiKey,
-  });
+  organization: 'org-ialr7176xUE4myKTzcivbiBn',
+  apiKey: ApiKey,
+});
 const openai = new OpenAIApi(configuration);
 
 export const fetchmodels = async () => {
-    const response = await openai.listModels()
-    return response.data;
-}
-
+  const response = await openai.listModels();
+  return response.data
+};
 
 export const fetchmessages = async (message, currentModel) => {
+  if (
+    currentModel === 'gpt-3.5-turbo' ||
+    currentModel === 'gpt-3.5-turbo-0301'
+  ) {
+    const completion = await openai.createChatCompletion({
+      model: currentModel,
+      max_tokens: 250,
+      messages: [{ role: 'user', content: message }],
+    });
+
+    return completion.data.choices[0].message.content;
+  } else {
     const response = await openai.createCompletion({
-        model: `${currentModel}`,
-        prompt: `${message}`,
-        max_tokens: 150,
-        temperature: 0.7,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-    return response.data.choices[0].text
-}
+      model: currentModel,
+      prompt: message,
+      max_tokens: 250,
+      temperature: 0.7,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+    });
+    return response.data.choices[0].text;
+  }
+};
