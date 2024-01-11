75c50f6291860b0722d01201fdc9ab5fd5806832
danieldotcomcoder <daniel_shdeed@hotmail.com>
2024-01-11 08:18:24 +0200

    Fix Model

--- src/fetchdata.js
+++ src/fetchdata.js
@@ -9,33 +9,15 @@ const openai = new OpenAIApi(configuration);
 
 export const fetchmodels = async () => {
   const response = await openai.listModels();
-  return response.data
+  return response.data;
 };
 
-export const fetchmessages = async (message, currentModel) => {
-  if (
-    currentModel === 'gpt-3.5-turbo' ||
-    currentModel === 'gpt-3.5-turbo-0301' ||
-    currentModel === 'gpt-4' ||
-    currentModel === 'gpt-4-0314'
-  ) {
-    const completion = await openai.createChatCompletion({
-      model: currentModel,
-      max_tokens: 250,
-      messages: [{ role: 'user', content: message }],
-    });
+export const fetchmessages = async (message) => {
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo-0301',
+    max_tokens: 250,
+    messages: [{ role: 'user', content: message }],
+  });
 
-    return completion.data.choices[0].message.content;
-  } else {
-    const response = await openai.createCompletion({
-      model: currentModel,
-      prompt: message,
-      max_tokens: 250,
-      temperature: 0.7,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    });
-    return response.data.choices[0].text;
-  }
+  return completion.data.choices[0].message.content;
 };
